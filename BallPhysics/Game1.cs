54a577469ca92f95ddad0fdc88b645436c7ef660
pawelplodzpl <pawel.kapusta@p.lodz.pl>
2019-05-06 00:11:49 +0200

    Initial Commit

--- BallPhysics/Game1.cs
+++ BallPhysics/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using C3.MonoGame;
+using System;
+using System.Collections.Generic;
 
 namespace BallPhysics
 {
@@ -11,10 +14,25 @@ namespace BallPhysics
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        
+        SpriteBatch HUDSpriteBatch;
+
+        List<Ball> greenBalls;
+        List<Ball> redBalls;
+
+        MouseState lastMouseState;
+        MouseState currentMouseState;
+
+        SpriteFont HUDFont;
+
+        string explanationText = "Press Left Mouse to place green balls, press Right Mouse to place red balls";
+
+        Random RNG = new Random();
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 800;
+            graphics.PreferredBackBufferWidth = 1000;
             Content.RootDirectory = "Content";
         }
 
@@ -28,6 +46,11 @@ namespace BallPhysics
         {
             // TODO: Add your initialization logic here
 
+            greenBalls = new List<Ball>();
+            redBalls = new List<Ball>();
+
+            this.IsMouseVisible = true;
+
             base.Initialize();
         }
 
@@ -39,8 +62,13 @@ namespace BallPhysics
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+            HUDSpriteBatch = new SpriteBatch(GraphicsDevice);
+
+            HUDFont = Content.Load<SpriteFont>("hudFontSmall");
 
             // TODO: use this.Content to load your game content here
+
+
         }
 
         /// <summary>
@@ -61,6 +89,33 @@ namespace BallPhysics
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            
+            // The active state from the last frame is now old
+            lastMouseState = currentMouseState;
+
+            // Get the mouse state relevant for this frame
+            currentMouseState = Mouse.GetState();
+
+            if (lastMouseState.LeftButton == ButtonState.Released && currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                greenBalls.Add(new Ball(BallTypes.Green, currentMouseState.Position.ToVector2(), RNG.Next(15, 30), applyGravity:true));
+            }
+
+            if (lastMouseState.RightButton == ButtonState.Released && currentMouseState.RightButton == ButtonState.Pressed)
+            {
+                redBalls.Add(new Ball(BallTypes.Red, currentMouseState.Position.ToVector2(), RNG.Next(30, 40)));
+            }
+
+
+            foreach (var ball in greenBalls)
+            {
+                ball.Update(gameTime);
+            }
+            foreach (var ball in redBalls)
+            {
+                ball.Update(gameTime);
+            }
+
 
             // TODO: Add your update logic here
 
@@ -74,6 +129,25 @@ namespace BallPhysics
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            foreach (var ball in greenBalls)
+            {
+                ball.Draw(spriteBatch);
+            }
+
+            foreach (var ball in redBalls)
+            {
+                ball.Draw(spriteBatch);
+            }
+
+
+            spriteBatch.End();
+
+            HUDSpriteBatch.Begin();
+            HUDSpriteBatch.DrawString(HUDFont, explanationText, new Vector2(20, 20), Color.White);
+            HUDSpriteBatch.End();
 
             // TODO: Add your drawing code here
 
---
+++ BallPhysics/Utils/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using C3.MonoGame;
+using System.Diagnostics;
+
+public enum BallTypes
+{
+    Green, 
+    Red
+}
+
+namespace BallPhysics
+{
+    public class Ball
+    {
+        public Vector2 Center { get; set; }
+        public float Radius { get; set; }
+        public BallTypes BallType { get; set; }
+        public float Mass { get; set; }
+        public bool ApplyGravity { get; set; }
+
+        Vector2 movementVector = Vector2.Zero;
+
+        public Ball(BallTypes ballType, Vector2 startPosition, float radius = 20.0f, float mass = 1.0f, bool applyGravity = false)
+        {
+            BallType = ballType;
+            Center = startPosition;
+            Radius = radius;
+            Mass = mass;
+            ApplyGravity = applyGravity;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Vector2 ballMovement = Vector2.Zero;
+
+            // Do all calculations
+
+            movementVector += ballMovement;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            var ballColor = (Color)typeof(Color).GetProperty(Enum.GetName(typeof(BallTypes), BallType)).GetValue(null);
+            spriteBatch.DrawCircle(Center, Radius, 360, ballColor, thickness:Radius);
+
+
+        }
+
+        public bool IsCollidingWith(Ball otherBall)
+        {
+            return Vector2.Distance(Center, otherBall.Center) <= (Radius + otherBall.Radius);
+
+        }
+
+        
+
+
+    }
+}
